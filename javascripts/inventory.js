1241221d839cc9ffcf482e8b5bd3e20fadb42beb
sofalere <sofalere@gmail.com>
2023-05-17 16:49:08 -0500

    refactoring

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -9,8 +9,9 @@ let inventory;
       document.querySelector("#order_date").innerText = date.toUTCString();
     },
     cacheTemplate: function() {
-      let inventoryItem = document.querySelector("#inventory_item").innerHTML;
-      this.template = Handlebars.compile(inventoryItem);
+      let inventoryItem = document.querySelector("#inventory_item");
+      this.template = Handlebars.compile(inventoryItem.innerHTML);
+      inventoryItem.remove();
     },
     add: function() {
       this.lastId++;
@@ -24,9 +25,9 @@ let inventory;
 
       return item;
     },
-    remove: function(idx) {
+    remove: function(id) {
       this.collection = this.collection.filter(function(item) {
-        return item.id !== idx;
+        return item.id !== id;
       });
     },
     get: function(id) {
@@ -42,8 +43,8 @@ let inventory;
       return found_item;
     },
     update: function(item) {
-      var id = this.findID(item),
-          collectionItem = this.get(id);
+      let id = this.findID(item);
+      let collectionItem = this.get(id);
 
       collectionItem.name = item.querySelector('[name^=item_name]').value;
       collectionItem.stock_number = item.querySelector('[name^=item_stock_number]').value;
@@ -52,10 +53,11 @@ let inventory;
     newItem: function(e) {
       e.preventDefault();
       let item = this.add();
-      let itemTemplate = this.template({ID: item.id});
-      let itemNode = document.createElement('tbody');
-      itemNode.innerHTML = itemTemplate;
-      document.querySelector("#inventory").appendChild(itemNode);
+      document.querySelector("#inventory").insertAdjacentHTML("beforeend", this.template({ID: item.id}))
+      // let itemTemplate = this.template({ID: item.id});
+      // let itemNode = document.createElement('tbody');
+      // itemNode.innerHTML = itemTemplate;
+      // document.querySelector("#inventory").appendChild(itemNode);
     },
     findParent: function(e) {
       return e.target.closest("tr");
@@ -65,30 +67,37 @@ let inventory;
     },
     deleteItem: function(e) {
       e.preventDefault();
-      var item = this.findParent(e)
-      this.remove(this.findID(item));
-      item.remove();
+      if (e.target.classList.contains('delete')) {
+        var item = this.findParent(e)
+        this.remove(this.findID(item));
+        item.remove();
+      }
     },
     updateItem: function(e) {
-      e.stopPropagation();
-      var item = this.findParent(e);
+      if (e.target.tagName === 'INPUT') {
+        var item = this.findParent(e);
 
-      this.update(item);
+        this.update(item);
+      }
     },
     bindEvents: function() {
       document.querySelector("#add_item").addEventListener("click", this.newItem.bind(this));
+      document.querySelector("#inventory")
+              .addEventListener('click', this.deleteItem.bind(this));
+      document.querySelector("#inventory")
+              .addEventListener('focusout', this.updateItem.bind(this));
       
-      document.querySelector("#inventory").addEventListener("click", e => {
-        [...e.currentTarget.querySelectorAll('a.delete')].forEach( input => {
-          input.addEventListener('click', this.deleteItem.bind(this));
-        })
-      })
+      // document.querySelector("#inventory").addEventListener("click", e => {
+      //   [...e.currentTarget.querySelectorAll('a.delete')].forEach( input => {
+      //     input.addEventListener('click', this.deleteItem.bind(this));
+      //   })
+      // }, true)
 
-      document.querySelector("#inventory").addEventListener("focusout", e => { 
-        [...e.currentTarget.querySelectorAll('input')].forEach( input => {
-          input.addEventListener('focusout', this.updateItem.bind(this));
-        });
-      });
+      // document.querySelector("#inventory").addEventListener("focusout", e => { 
+      //   [...e.currentTarget.querySelectorAll('input')].forEach( input => {
+      //     input.addEventListener('focusout', this.updateItem.bind(this));
+      //   });
+      // }, true);
     },
     init: function() {
       this.setDate();
