0d80a7bc135ff781e95d6d36b19501844e8bf968
sofalere <sofalere@gmail.com>
2023-05-17 16:12:24 -0500

    $item in delete and update

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -7,14 +7,11 @@ var inventory;
     setDate: function() {
       var date = new Date();
       document.querySelector("#order_date").innerText = date.toUTCString();
-      // $("#order_date").text(date.toUTCString());
     },
     cacheTemplate: function() {
       let inventoryItem = document.querySelector("#inventory_item");
       this.template = inventoryItem.innerHTML;
       inventoryItem.remove();
-      // var $iTmpl = $("#inventory_item").remove();
-      // this.template = $iTmpl.html(); 
     },
     add: function() {
       this.lastId++;
@@ -45,13 +42,13 @@ var inventory;
 
       return found_item;
     },
-    update: function($item) {
-      var id = this.findID($item),
-          item = this.get(id);
+    update: function(item) {
+      var id = this.findID(item),
+          collectionItem = this.get(id);
 
-      item.name = $item.find("[name^=item_name]").val();
-      item.stock_number = $item.find("[name^=item_stock_number]").val();
-      item.quantity = $item.find("[name^=item_quantity]").val();
+      collectionItem.name = item.querySelector('[name^=item_name]').value;
+      collectionItem.stock_number = item.querySelector('[name^=item_stock_number]').value;
+      collectionItem.quantity = item.querySelector('[name^=item_quantity]').value;
     },
     newItem: function(e) {
       e.preventDefault();
@@ -60,33 +57,40 @@ var inventory;
       let itemNode = document.createElement('tbody');
       itemNode.innerHTML = itemTemplate;
       document.querySelector("#inventory").appendChild(itemNode);
-      
-      // var item = this.add(),
-      //     $item = $(this.template.replace(/ID/g, item.id));
-
-      // $("#inventory").append($item);
     },
     findParent: function(e) {
-      return $(e.target).closest("tr");
+      return e.target.closest("tr");
     },
-    findID: function($item) {
-      return +$item.find("input[type=hidden]").val();
+    findID: function(item) {
+      return +item.querySelector('[type=hidden]').value;
     },
     deleteItem: function(e) {
       e.preventDefault();
-      var $item = this.findParent(e).remove();
-
-      this.remove(this.findID($item));
+      var item = this.findParent(e)
+      this.remove(this.findID(item));
+      item.remove();
     },
     updateItem: function(e) {
-      var $item = this.findParent(e);
+      e.stopPropagation();
+      var item = this.findParent(e);
 
-      this.update($item);
+      this.update(item);
     },
     bindEvents: function() {
-      $("#add_item").on("click", $.proxy(this.newItem, this));
+      document.querySelector("#add_item").addEventListener("click", this.newItem.bind(this));
+      
+      document.querySelector("#inventory").addEventListener("click", e => {
+        [...e.currentTarget.querySelectorAll('a delete')].forEach( input => {
+          input.addEventListener('click', this.deleteItem.bind(this));
+        })
+      })
       $("#inventory").on("click", "a.delete", $.proxy(this.deleteItem, this));
-      $("#inventory").on("blur", ":input", $.proxy(this.updateItem, this));
+      
+      document.querySelector("#inventory").addEventListener("focusout", e => { 
+        [...e.currentTarget.querySelectorAll('input')].forEach( input => {
+          input.addEventListener('focusout', this.updateItem.bind(this));
+        });
+      });
     },
     init: function() {
       this.setDate();
@@ -96,7 +100,10 @@ var inventory;
   };
 })();
 
+
+// document.addEventListener("DOMContentLoaded", e => {
 $($.proxy(inventory.init, inventory));
+// });
 
 /*
 - remove jquery function 
